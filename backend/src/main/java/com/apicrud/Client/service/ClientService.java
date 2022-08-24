4b59a093e6d7d585aaee377369bc849fe0bf4a19
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-08-24 19:40:19 -0300

    Client Delete

--- backend/src/main/java/com/apicrud/Client/service/ClientService.java
+++ backend/src/main/java/com/apicrud/Client/service/ClientService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.apicrud.Client.dto.ClientDTO;
 import com.apicrud.Client.entities.Client;
 import com.apicrud.Client.repositories.ClientRepository;
+import com.apicrud.Client.service.exceptions.DataBaseException;
 import com.apicrud.Client.service.exceptions.ResourceNotFoundException;
 
 @Service
@@ -52,6 +55,17 @@ public class ClientService {
 			return new ClientDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found" + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataBaseException("Integrity violation");
 		}
 	}
 
