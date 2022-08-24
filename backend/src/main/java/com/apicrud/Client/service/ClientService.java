846d636cdacd2d954c4adbcc24db91b6953bb7f7
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-08-24 19:08:46 -0300

    findById com Exceção

--- backend/src/main/java/com/apicrud/Client/service/ClientService.java
+++ backend/src/main/java/com/apicrud/Client/service/ClientService.java
@@ -1,5 +1,7 @@
 package com.apicrud.Client.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.apicrud.Client.dto.ClientDTO;
 import com.apicrud.Client.entities.Client;
 import com.apicrud.Client.repositories.ClientRepository;
+import com.apicrud.Client.service.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -20,5 +23,12 @@ public class ClientService {
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list =  repository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
 	}
 }
